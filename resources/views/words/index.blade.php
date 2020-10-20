b2ce72662825218de8ba556a6d1b546c9081f2e6
oneten <onetenten6@gmail.com>
2020-10-20 12:39:59 +0000

    debag on

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
             <p>小説の登場人物や国などの組織、能力や武器、動物など様々な用語が並ぶページです。
-            用語をクリックすると詳細ページに飛べます。</p>
+            <br>用語をクリックすると詳細ページに飛べます。</p>
             @if (count($words) > 0)
                 @foreach ($words as $word)
                         <p>{!! link_to_route('words.show', $word->name , ['word' => $word->id]) !!}</p>
