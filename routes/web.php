f63b99c413fc97d1cc6c87fb7d5bd1b7dfca4871
oneten <onetenten6@gmail.com>
2020-10-31 14:44:37 +0000

    sendmail

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes(['verify' => true]);
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 // 認証
